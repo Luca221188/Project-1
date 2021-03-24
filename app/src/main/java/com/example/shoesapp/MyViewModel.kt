780bfce282c99f967c80dd139d13f29a313cd730
Luca221188 <34077175+Luca221188@users.noreply.github.com>
2021-03-24 11:57:25 +0000

    my second commit

--- app/src/main/java/com/example/shoesapp/MyViewModel.kt
+++ app/src/main/java/com/example/shoesapp/MyViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.shoesapp.model.Shoe
 
+//Did I get through it..?
 class MyViewModel: ViewModel() {
     private val _shoes: MutableLiveData<MutableList<Shoe>> = MutableLiveData()
     val shoes: LiveData<MutableList<Shoe>>
